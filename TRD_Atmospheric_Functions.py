- fixed unit conversion error in emission_plotting - added "cutoff angle" integration, photon density flux calculation, and related plotting in interpolated_spectral_radiance.py - housekeeping (deleted unused, outdated file. renamed Plotting to more descriptive name)

TRD_Atmospheric_Functions.py
@@ -26,7 +26,7 @@ def convert_from(array_in, units_in, units_out, corresponding_xs=None):
             return 1e-2 * array_in / (h*c/q)
             # [cm-1/m-1][Y/cm-1]/[J.s][m.s-1][eV/J] --> [Y/m-1]/[eV.m] --> [Y/eV]
 
-def load_file_as_xarray(cwv = 10):#, convert_to_wavelength = True):
+def load_file_as_xarray(cwv = 10):
     filename = os.path.join('simulations_telfer', f'telfer_australia_cwv{cwv}.txt')
     data = np.loadtxt(filename)
 
@@ -38,13 +38,6 @@ def load_file_as_xarray(cwv = 10):#, convert_to_wavelength = True):
     ]
     ds = xr.DataArray(data[:, 1:], coords={'wavenumber': data[:, 0],
                                            'column': column_labels[1:]}, dims=['wavenumber', 'column'])
-
-    # if convert_to_wavelength:
-    #     # converting wavenumber to wavelength: just 1/wavenumber. But also need to adjust flux/radiance since these
-    #     # are per cm-1 (Jacobian transformation)
-    #     ds = 1e-4 * ds * (ds['wavenumber']) ** 2  # [W.cm-2/cm-1]*[cm-1]^2 = [W.cm-2/cm] --> *1e-4 --> [W.cm-2/um]
-    #     ds = ds.rename({'wavenumber': 'wavelength'})
-    #     ds.coords['wavelength'] = 1e4 / ds.coords['wavelength']  # [1/cm-1]=[cm] ---> *1e4 --> [um]
 
     return ds
 
@@ -72,7 +65,7 @@ def retrieve_downwellingrad_as_nparray(cwv=10, x_vals = 'photon energies'):
     elif x_vals == 'photon energies':
         x_array = convert_from(wavnums, units_in='wavenumber [cm-1]', units_out='photon energy [eV]')
         dat_3D_per_um = convert_from(dat_3D, units_in='per wavenumber [/cm-1]', units_out='per photon energy [/eV]')   # [W.cm-2.sr-1/cm-1] --> [W.cm-2.sr-1/eV]
-        dat_3D_converted = 1e4 * dat_3D_per_um  # [W.m-2.sr-1/um]
+        dat_3D_converted = 1e4 * dat_3D_per_um  # [W.m-2.sr-1/eV]
 
     return {x_vals:x_array, 'downwelling rad':dat_3D_converted}
 
@@ -95,8 +88,8 @@ def retrieve_downwelling_in_Wm2(cwv = 10, in_wavelength = False):
                                                units_in='per wavenumber [/cm-1]', units_out='per wavelength [/um]',
                                                corresponding_xs = x_wavnums) # [W.cm-2/cm-1] --> [W.cm-2/um]
         downwelling_flux = 1e4 * downwelling_flux_cm2_um  # [W.cm-2/um] --> [W.m-2/um]
-        wavns = convert_from(x_wavnums, units_in='wavenumber [cm-1]', units_out='wavelength [um]')
-        return {'wavelengths': wavns, 'downwelling flux': downwelling_flux}  # {[um], [W.m-2/um]}
+        wls = convert_from(x_wavnums, units_in='wavenumber [cm-1]', units_out='wavelength [um]')
+        return {'wavelengths': wls, 'downwelling flux': downwelling_flux}  # {[um], [W.m-2/um]}
 
     else:  # in wavenumber
         downwelling_flux = 1e4 * d_xarray.sel(column='downwelling_flux')  # [W.cm-2/cm-1] --> [W.m-2/cm-1]
@@ -123,13 +116,27 @@ def retrieve_downwelling_in_particleflux(cwv=10):
     return {'photon energies':phot_energies_eV, 'downwelling photon flux':dwf_part}
 
 
+def interpolate_spectralrad_by_angle(cwv, x_vals = 'photon energies', number_of_angles = 100):
+    existing_dat = retrieve_downwellingrad_as_nparray(cwv, x_vals)
+    xs, dat_3D = existing_dat[x_vals], existing_dat['downwelling rad']  # [eV], [W.m-2/sr.eV] or [um], [W.m-2/sr.um]
+    angle_array = np.linspace(0, 90, number_of_angles)
+    dat_3D_int = []
+    for xi in range(len(xs)):
+        # for each wavelength, interpolate values for angles specified
+        dat_3D_int += [np.interp(x=angle_array, xp=[0, 53, 70], fp=dat_3D[:, xi])]
+
+    dat_3D_int = np.array(dat_3D_int)  # rows correspond to x values (photon energies or wavelengths)
+    dat_3D_int = dat_3D_int.transpose()  # rows correspond to angles
+
+    return {'angles (rows)':angle_array, f'{x_vals} (columns)':xs, 'int rad':dat_3D_int}
+
 
 def planck_dist(Eph, mu, kT):
     '''
     :param Eph: Particular photon energy, in eV
     :param mu: Fermi level splitting, in eV
     :param kT: in eV
-    :return: Photon flux [s-1.m-2] for the given eV, calculated using generalized Planck's law (blackbody with temp T if mu=0).
+    :return: Photon flux for the given eV, in [s-1.m-2.eV-1], calculated using generalized Planck's law (blackbody with temp T if mu=0).
     '''
     return ((2*np.pi)/(c**2*(h/q)**3))* Eph**2/(np.exp((Eph-mu)/kT)-1)
 

emission_plotting.py
@@ -20,7 +20,7 @@ col_dict = {'planck_emit':'skyblue', 'planckheavy_emit':'darkblue', 'planck_env_
             'boltzmann_emit2':'springgreen', 'downwell_env':'lightpink', 'downwellheavy_env':'deeppink'}
 
 
-# Plot effect of mu on TRD emission
+# ------------ EFFECT OF MU ON TRD EMISSION CURVE ------------
 fig, axs = plt.subplots(1,1, layout='tight', sharey='all')
 
 Egs = np.arange(0.01, 0.20, 0.05)
@@ -32,7 +32,7 @@ ax = axs
 mu_spec = -0.025
 cmap = matplotlib.colormaps['plasma']
 for mi, mu in enumerate(mus):
-    spec_pflux_planck = ((2*np.pi)/(c**2*(h/q)**3))* planck_dist(Ephs, mu, kT_c_eV)  # [m-2.s-2/eV]
+    spec_pflux_planck = planck_dist(Ephs, mu, kT_c_eV)  # [m-2.s-2/eV]
 
     mu_colour = cmap(0.2 + 0.8 * mi / len(mus))
     ax.plot(Ephs, spec_pflux_planck, label=f'$\mu$ = {mu}', color=mu_colour)
@@ -60,20 +60,20 @@ ax.legend()
 # plt.ylabel('(Spectral) Photon Density Flux [$\mathrm{m^{-2}.s^{-1}.eV^{-1}}$]')
 #
 # plt.ylim([0,3.8*1e23])
-# plt.show()
+
 
 
 # Downwelling - W.m-2 vs Eph, to photon flux vs Eph
-# downwell_dat_dict = retrieve_downwelling_in_particleflux(10)
-# fig, axs = plt.subplots(1,2, layout='tight')
-# Ephs = downwell_dat_dict['photon energies']
-# axs[0].plot(Ephs, downwell_dat_dict['downwelling photon flux']*Ephs, c='crimson')
-# axs[1].plot(Ephs, downwell_dat_dict['downwelling photon flux'], c=col_dict['downwellheavy_env'])
-#
-# axs[0].set_xlabel('Photon Energy, E$_{ph}$ [eV]')
-# axs[1].set_xlabel('Photon Energy, E$_{ph}$ [eV]')
-# axs[0].set_ylabel('(Spectral) Power Density Flux [$\mathrm{W.m^{-2}/eV}$]')
-# axs[1].set_ylabel('(Spectral) Photon Density Flux [$\mathrm{m^{-2}.s^{-1}/eV}$]')
+downwell_dat_dict = retrieve_downwelling_in_particleflux(10)
+fig, axs = plt.subplots(1,2, layout='tight')
+Ephs = downwell_dat_dict['photon energies']
+axs[0].plot(Ephs, downwell_dat_dict['downwelling photon flux']*Ephs*q, c='crimson')  # [m-2.s-1/eV]*[eV]*[J/eV] --> [W.m-2/eV]
+axs[1].plot(Ephs, downwell_dat_dict['downwelling photon flux'], c=col_dict['downwellheavy_env'])
+
+axs[0].set_xlabel('Photon Energy, E$_{ph}$ [eV]')
+axs[1].set_xlabel('Photon Energy, E$_{ph}$ [eV]')
+axs[0].set_ylabel('(Spectral) Power Density Flux [$\mathrm{W.m^{-2}/eV}$]')
+axs[1].set_ylabel('(Spectral) Photon Density Flux [$\mathrm{m^{-2}.s^{-1}/eV}$]')
 
 
 plt.show()

interpolated_spectral_radiance.py
@@ -1,50 +1,84 @@
 import matplotlib.pyplot as plt
 from TRD_Atmospheric_Functions import *
 
+# make heatmap
+# xstr = 'photon energies'
+# int_dict = interpolate_spectralrad_by_angle(cwv=10, x_vals=xstr, number_of_angles=100)
+# xs = int_dict[f'{xstr} (columns)']
+# dat_3D_int = int_dict['int rad']
+# angle_array = int_dict['angles (rows)']
+# fig, axs = plt.subplots(1,2, layout='tight')
+#
+# h_ax = axs[0]
+# hmap = h_ax.pcolor(xs, angle_array, dat_3D_int, cmap='plasma', shading='nearest')  # heatmap
+# cbar = plt.colorbar(hmap)
+# cbar.ax.tick_params(labelsize=10)
+# cbar.ax.set_ylabel(r'Spectral Rad [$\mathrm{W.m^{-2}.sr^{-1}/um}$]')
+# h_ax.set_ylabel('Solid Angle [sr]')
+# h_ax.set_xlabel(xstr)
+#
+#
+# # take slices of heatmap, plot separately as line plot
+# slice_ax = axs[1]
+# if xstr == 'wavelengths':
+#     slice_xs = [6,9.5,13,18]
+#     s_idxs = len(xs) - np.searchsorted(np.flip(xs), slice_xs)  # indexes of slice wavelengths
+# elif xstr == 'photon energies':
+#     slice_xs = [0.08, 0.12, 0.15, 0.2]
+#     s_idxs = np.searchsorted(xs, slice_xs)
+#
+# for si, swav in zip(s_idxs, slice_xs):
+#     # plot spectral rad vs angle for a particular photon energy / wavelength
+#     spectral_rad_slice = dat_3D_int[:,si]
+#     slice_ax.plot(angle_array, spectral_rad_slice, label=f'{swav}')
+#
+#     h_ax.plot(2*[swav], [angle_array[0],angle_array[-1]], '--')
+#
+# slice_ax.set_xlabel('Solid Angle [sr]')
+# slice_ax.set_ylabel('Spectral Rad [$\mathrm{W.m^{-2}.sr^{-1}/um}$]')
+# slice_ax.legend()
+
+
+
+# integrate over solid angle, using heaviside weighing and different angles of acceptance
+# cutoff angle
 fig, axs = plt.subplots(1,2, layout='tight')
 
-dwrad_dict = retrieve_downwellingrad_as_nparray(10, x_vals = 'wavelengths')
-wavs = dwrad_dict['wavelengths']
-dat_3D = dwrad_dict['downwelling rad']
+xstr = 'photon energies'
+int_dict = interpolate_spectralrad_by_angle(cwv=10, x_vals=xstr, number_of_angles=100)
+Ephs = int_dict[f'{xstr} (columns)']
+dat_3D_int = int_dict['int rad']
+angle_array = int_dict['angles (rows)']
 
-# interpolate data at different angles
-angle_array = np.linspace(0,90,10)
-dat_3D_int = []
-for wi in range(len(wavs)):
-    # for each wavelength, interpolate values for angles specified
-    dat_3D_int += [np.interp(x=angle_array, xp=[0,53,70], fp=dat_3D[:,wi])]
+Egs = np.arange(0.062, 0.20, 0.002)
 
-dat_3D_int = np.array(dat_3D_int)  # rows correspond to wavelengths
-dat_3D_int = dat_3D_int.transpose()  # rows correspond to angles
+cutoff_angles = np.arange(10,95,20)
+for cutoff_angle in cutoff_angles:
+    # downwelling photon flux (integrate over angles that are accepted)
+    hvs = np.heaviside(cutoff_angle-angle_array, 1)
+    rad_with_heaviside = np.transpose(np.transpose(dat_3D_int)*hvs)
+    spectral_power_density = 2*np.trapz(rad_with_heaviside, np.radians(angle_array), axis=0)
 
+    axs[0].plot(Ephs, spectral_power_density, label=cutoff_angle)
 
-# make heatmap
-h_ax = axs[0]
-hmap = h_ax.pcolor(wavs, angle_array, dat_3D_int, cmap='plasma', shading='nearest')  # heatmap
-cbar = plt.colorbar(hmap)
-cbar.ax.tick_params(labelsize=10)
-cbar.ax.set_ylabel(r'Spectral Rad [$\mathrm{W.m^{-2}.sr^{-1}/um}$]')
-h_ax.set_ylabel('Solid Angle [sr]')
-h_ax.set_xlabel('Wavelength [um]')
+    phot_flux = spectral_power_density / (Ephs*q)  # [W.m-2/eV] / [eV][J/eV] --> [s-1.m-2/eV]
+    dw_dict = {'photon energies':Ephs, 'downwelling photon flux':phot_flux}
+    Ndot_vs_Eg = np.vectorize(Ndot_downwellheaviside)(Egs, dw_dict)
+    axs[1].plot(Egs, Ndot_vs_Eg)
 
+# diffusivity approx, for comparison
+dw_diffusitivtyapprox = retrieve_downwelling_in_particleflux(cwv=10)
+Ephs_diff = dw_diffusitivtyapprox['photon energies']
+SPDF_diff = dw_diffusitivtyapprox['downwelling photon flux']*Ephs*q
+axs[0].plot(Ephs_diff, SPDF_diff, ':k', label='Diffusivity approx (53$^\circ \\times \pi$)')
+axs[1].plot(Egs, np.vectorize(Ndot_downwellheaviside)(Egs, dw_diffusitivtyapprox), ':k')
 
-# take slices of heatmap, plot separately as line plot
-slice_ax = axs[1]
-slice_wavelengths = [6,9.5,13,18]
-s_idxs = len(wavs) - np.searchsorted(np.flip(wavs),slice_wavelengths)  # indexes of slice wavelengths
-for si, swav in zip(s_idxs, slice_wavelengths):
-    spectral_rad_slice = dat_3D_int[:,si]
-    slice_ax.plot(angle_array, spectral_rad_slice, label=f'{swav} um')
+axs[0].legend()
+axs[0].set_ylabel('Spectral Power Density Flux [W.m$^{-2}$/eV]')
+axs[0].set_xlabel('Photon Energy, E$_{ph}$ [eV]')
 
-    h_ax.plot(2*[swav], [angle_array[0],angle_array[-1]], '--')
-
-slice_ax.set_xlabel('Solid Angle [sr]')
-slice_ax.set_ylabel('Spectral Rad [$\mathrm{W.m^{-2}.sr^{-1}/um}$]')
-slice_ax.legend()
-
-
-# integrate
-
+axs[1].set_ylabel('Photon Density Flux, $\dot{\mathrm{N}}$ [m$^{-2}$.s$^{-1}]$')
+axs[1].set_xlabel('Bandgap, E$_g$ [eV]')
 
 
 